99759765b3db7ed14147776a8284a0277f2a575d
GodzTitan <aafaqqadri7792@gmail.com>
2026-06-03 11:15:15 +0000

    DataType

---
+++ 01_basics/01_variabales/03_datatype.js
@@ -0,0 +1,30 @@
+"use strigt"; // treat all JS. code as newer version
+// alert(3+3) // ew are using node.js, not browser
+
+
+// console.log(3 + 3) console.log("Ashfaq") this is not allowed bt JS
+ console.log(3 
+    + 3) // code readability should br hing
+
+    console.log("Ashfaq");
+    
+ let name = "ashfaq";
+ let age = 25;
+ let isLoggedIn = false
+ let state;
+
+ // number => 2 to power 53
+ // bigint
+ // string => ""
+ // boolean = true/false
+ // null => standalone value
+ // undefined =>
+ // sybol => unique 
+
+ // object 
+
+
+
+ console.log(typeof ""); // string => ""
+ console.log(typeof null); // null => object
+ console.log(typeof undefined); // // undefined =>  undefined
