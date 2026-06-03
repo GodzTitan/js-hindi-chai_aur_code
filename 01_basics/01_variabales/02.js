76b2afd96635b71112687c298d0571c335c34f7f
GodzTitan <aafaqqadri7792@gmail.com>
2026-06-03 10:40:35 +0000

    javascript variables types

---
+++ 01_basics/01_variabales/02.js
@@ -0,0 +1,24 @@
+const accountId = 778692;
+let accountEmail = "ashfaq@gmail.com";
+var accountPassword = "0313";
+accountCity = "Aliya Bada";
+
+// accountId = 2 // This Is A Not Allowed By Js
+
+accountEmail = "Ashfaq@gmail.com"
+accountPassword = 313;
+accountCity = "Jamanger";
+console.log(accountEmail);
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+
+
+// ┌─────────┬────────────────────┐
+// │ (index) │ Values             │
+// ├─────────┼────────────────────┤
+// │ 0       │ 778692             │
+// │ 1       │ 'Ashfaq@gmail.com' │
+// │ 2       │ 313                │
+// │ 3       │ 'jamanger'         │
+// └─────────┴────────────────────┘
+
