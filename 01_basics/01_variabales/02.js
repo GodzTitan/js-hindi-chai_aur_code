5b44cd5e08f31d0ce7d5aca7e73d3acc5f1fefa5
GodzTitan <aafaqqadri7792@gmail.com>
2026-06-03 10:49:39 +0000

    javascript variables types

--- 01_basics/01_variabales/02.js
+++ 01_basics/01_variabales/02.js
@@ -2,6 +2,7 @@ const accountId = 778692;
 let accountEmail = "ashfaq@gmail.com";
 var accountPassword = "0313";
 accountCity = "Aliya Bada";
+let accoutState;
 
 // accountId = 2 // This Is A Not Allowed By Js
 
@@ -9,7 +10,7 @@ accountEmail = "Ashfaq@gmail.com"
 accountPassword = 313;
 accountCity = "Jamanger";
 console.log(accountEmail);
-console.table([accountId,accountEmail,accountPassword,accountCity])
+console.table([accountId,accountEmail,accountPassword,accountCity,accoutState])
 
 
 
@@ -19,6 +20,10 @@ console.table([accountId,accountEmail,accountPassword,accountCity])
 // │ 0       │ 778692             │
 // │ 1       │ 'Ashfaq@gmail.com' │
 // │ 2       │ 313                │
-// │ 3       │ 'jamanger'         │
+// │ 3       │ 'Jamanger'         │
+// │ 4       │ undefined          │
 // └─────────┴────────────────────┘
 
+/*
+do not use var
+*/
