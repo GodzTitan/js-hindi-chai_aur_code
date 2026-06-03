fe52363700718c7313af8dab89885d5d3613c33a
GodzTitan <aafaqqadri7792@gmail.com>
2026-06-03 13:38:12 +0000

    conversion

---
+++ 01_basics/01_variabales/04_convesion.js
@@ -0,0 +1,32 @@
+let score = null;
+
+
+// const {score} = req.body
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+
+console.log(typeof(valueInNumber));
+console.log( valueInNumber);
+
+
+// "23" => 23
+// "23abc" => NaN
+// true => 1; false => 0 
+
+
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+// true => 1; false => 0 
+// "" => false
+// "js" => true
+
+
+let sumNumber = 23;
+let stringNumber = String(sumNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
